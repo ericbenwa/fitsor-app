0e4a1725394698e19384078d5b15876ee3bb0776
Eric Benoit <eric@goinvo.com>
2013-10-31 21:49:54 -0400

    Functional via URL params

--- app/api/exercises/post.php
+++ app/api/exercises/post.php
@@ -1,5 +1,6 @@
 <?php
-// mysql_query("SET time_zone = 'US/New_York'");
+mysql_query("SET time_zone = 'US/New_York'");
+
 /*
 foreach ($_REQUEST as $key => $value)
 {
@@ -7,31 +8,37 @@ foreach ($_REQUEST as $key => $value)
 		$pullups_total = $value;
 	}
 }
-
 echo $pullups_total;
 */
 
+// Settings to connect to the database
 $username = "ebenoitc_fitsor";
 $password = "fitsor";
 $database = "ebenoitc_fitsor";
 $localhost = "localhost";
 
+// URL parameters to interact with database
+$user_id = $_GET['user_id'];
+$device_udid = $_GET['device_udidd'];
+$exercise_type_id = $_GET['exercise_type_id'];
+$repetitions = $_GET['repetitions'];
+
 // Check connection
 if (mysql_connect($localhost, $username, $password))
   {
-  	@mysql_select_db($database) or die("Unable to select database");
+  	@mysql_select_db($database) or die ("Unable to select database");
 
   	$query = "
-      INSERT INTO exercises (id, user_id, device_udid, exercise_type_id, repetitions, exercise_time, created_at, updated_at) VALUES (3, SELECT(id FROM users WHERE id=1), SELECT(id FROM devices WHERE id=1), SELECT(id FROM exercise_types WHERE id=1), 209, now(), now(),now());
+		INSERT INTO exercises (user_id, device_udid, exercise_type_id, repetitions, exercise_time, created_at, updated_at)
+		SELECT users.id, devices.id, exercise_types.id, $repetitions, NOW(), NOW(), NOW()
+		FROM users, devices, exercise_types
+		WHERE users.id = $user_id AND devices.id = $device_udid AND exercise_types.id = $exercise_type_id;
     ";
 
-          // INSERT INTO exercises 
-      // VALUES 
-      // (NULL, 1, 1, 1, '89', now(), now(), now())
     echo($query);
     echo(mysql_error());
   	$result = mysql_query($query);
   } else {
-  	echo('not working');
+  	echo('Not Working');
   }
 ?>
