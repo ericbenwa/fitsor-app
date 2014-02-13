ac386789434f24fd1b39aedf277c7d00fece72df
Eric Benoit <eric@goinvo.com>
2014-02-13 00:26:01 -0500

    Revised bootstrap nav for responsiveness

--- laravel/app/views/layouts/main.blade.php
+++ laravel/app/views/layouts/main.blade.php
@@ -11,32 +11,42 @@
 
 <body>
 
-	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-		<div class="navbar-inner">
-			<div class="container">
-				<ul class="nav navbar-nav">
-					<li><a href="#">Fitsor</a></li>
-					@if(!Auth::check())
-					<li>{{ HTML::link('users/register', 'Get Started') }}</li>   
-					<li>{{ HTML::link('users/login', 'Sign In') }}</li>
-					@else
-					<li>{{ HTML::link('users/dashboard', 'Dashboard') }}</li>
-					<li>{{ HTML::link('users/profile', 'My Profile') }}</li>
-					<li>{{ HTML::link('users/logout', 'Log Out') }}</li>
-					@endif
-					<li><a href="mailto:hello@fitsor.com?subject=Feedback">Feedback</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
+	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Fitsor</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+			@if(!Auth::check())
+			<li>{{ HTML::link('about', 'About') }}</li>
+			<li>{{ HTML::link('users/register', 'Get Started') }}</li>   
+			<li>{{ HTML::link('users/login', 'Sign In') }}</li>
+			@else
+			<li>{{ HTML::link('users/dashboard', 'Dashboard') }}</li>
+			<li>{{ HTML::link('users/profile', 'My Profile') }}</li>
+			<li>{{ HTML::link('users/logout', 'Log Out') }}</li>
+			@endif
+			<li><a href="mailto:hello@fitsor.com?subject=Feedback">Feedback</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
 
 	<div class="container">
-		<!-- @if(Session::has('message'))
-		<p class="alert">{{ Session::get('message') }}</p>
-		@endif -->
-
 		{{ $content }}
 	</div>
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    {{ HTML::script('https://code.jquery.com/jquery-1.10.2.min.js')}}
+    {{ HTML::script('packages/bootstrap/js/bootstrap.min.js')}}
 
 </body>
 </html>
