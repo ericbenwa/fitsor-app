9aad5dbfae85cb17c128d6e1a1f663542b8dcdb4
Eric Benoit <eric@goinvo.com>
2014-02-16 01:03:07 -0500

    Going to rename exercise_type column but didn't - TBD

---
+++ laravel/app/database/migrations/2014_02_15_234116_rename_exercise_type_column.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class RenameExerciseTypeColumn extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Schema::table('exercise_types', function($table) {
+		// 	$table->exercise_types('')
+		// });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
