816da6921acfeccd594e7c39babef816343adaa2
Eric Benoit <eric@goinvo.com>
2014-02-25 18:55:33 -0500

    Added dashboard and public profiles, fixed references to 'users'

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,16 @@
 <?php
 
 // Application Routes
-Route::controller('users', 'UsersController');
+Route::controller('user', 'UsersController');
 
-// Marketing Routes
+Route::get('dashboard', array('as' => 'dashboard', function() {
+	return View::make('user/dashboard')->with('user', Auth::user());
+}));
+
+Route::get('user/{id}', function($id)
+{
+  return User::find($id);
+});
+
+// Marketing Routes (something messed up if this controller is not at the bottom...?)
 Route::controller('/', 'MarketingController');
